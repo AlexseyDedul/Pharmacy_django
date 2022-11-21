826a9ae02724245b100e61d18ee3656cce96b66d
dedul.an <dedul.an@prj365.com>
2022-11-21 15:37:11 +0300

    added templates

--- pharmacy/app/views.py
+++ pharmacy/app/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+menu = ['Аптеки', 'Лекарственные средства', 'Заявки', "О сайте"]
+
+
+def index(request):
+    return render(request, 'pharmacy/index.html', {'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'pharmacy/about.html', {'title': 'О сайте'})
